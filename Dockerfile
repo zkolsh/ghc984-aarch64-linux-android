abdfe851af8563d98dad77062c2a822dfaba98c8
n.x <zkolsh@proton.me>
2025-12-22 21:41:45 -0300

    use docker idk

---
+++ Dockerfile
@@ -0,0 +1,33 @@
+FROM ubuntu:22.04
+
+ENV DEBIAN_FRONTEND=noninteractive
+ENV ANDROID_API=35
+ENV TARGET=aarch64-linux-android
+
+RUN apt-get update && apt-get install -y \
+    build-essential autoconf automake libtool \
+    curl git python3 gperf \
+    llvm-15 clang \
+    xz-utils ca-certificates \
+    && rm -rf /var/lib/apt/lists/*
+
+RUN mkdir /ndk && \
+    curl -L https://dl.google.com/android/repository/android-ndk-r26d-linux.zip \
+    -o /ndk/ndk.zip && \
+    apt-get update && apt-get install -y unzip && \
+    unzip /ndk/ndk.zip -d /ndk && \
+    mv /ndk/android-ndk-r26d /android-ndk && \
+    rm -rf /ndk
+
+ENV ANDROID_NDK_HOME=/android-ndk
+ENV PATH=$ANDROID_NDK_HOME/toolchains/llvm/prebuilt/linux-x86_64/bin:$PATH
+
+RUN curl -L https://downloads.haskell.org/ghc/9.8.4/ghc-9.8.4-x86_64-deb11-linux.tar.xz \
+    | tar -xJ && \
+    cd ghc-9.8.4 && \
+    ./configure --prefix=/opt/ghc && \
+    make install
+
+ENV PATH=/opt/ghc/bin:$PATH
+
+WORKDIR /build
---
+++ build-ghc984-aarch64-linux-android.sh
@@ -0,0 +1,47 @@
+#!/usr/bin/env bash
+set -eux
+
+PREFIX=/opt/android-bridge
+TARGET=aarch64-linux-android
+API=21
+CC=${TARGET}${API}-clang
+
+curl -LO https://ftp.gnu.org/pub/gnu/libiconv/libiconv-1.17.tar.gz
+tar -xzf libiconv-1.17.tar.gz
+cd libiconv-1.17
+./configure --host=$TARGET --prefix=$PREFIX --disable-shared --enable-static CC=$CC
+make -j$(nproc)
+make install
+cd ..
+
+curl -LO https://ftp.gnu.org/pub/gnu/ncurses/ncurses-6.4.tar.gz
+tar -xzf ncurses-6.4.tar.gz
+cd ncurses-6.4
+./configure --host=$TARGET --prefix=$PREFIX \
+  --with-shared=no --without-debug \
+  --without-ada --without-cxx-binding \
+  --enable-widec CC=$CC
+make -j$(nproc)
+make install
+cd ..
+
+git clone --recursive -b ghc-9.8.4-release https://github.com/ghc/ghc.git ghc
+cd ghc
+
+./boot
+./configure \
+  --target=$TARGET \
+  --with-intree-gmp \
+  --with-system-libffi=no \
+  --with-iconv-includes=$PREFIX/include \
+  --with-iconv-libraries=$PREFIX/lib \
+  --with-curses-includes=$PREFIX/include \
+  --with-curses-libraries=$PREFIX/lib \
+  --with-clang=$(which clang) \
+  --with-ar=llvm-ar \
+  --with-nm=llvm-nm \
+  --with-ranlib=llvm-ranlib \
+  CONF_CC_OPTS_STAGE2="-I$PREFIX/include" \
+  CONF_GCC_LINKER_OPTS_STAGE2="-L$PREFIX/lib"
+
+hadrian/build -j$(nproc) --flavour=quick-cross binary-dist
