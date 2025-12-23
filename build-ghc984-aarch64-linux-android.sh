d773e0f67bd0e056f2eb57afc33ef1b70948acfb
n.x <zkolsh@proton.me>
2025-12-23 15:19:33 -0300

    don't touch build files

--- build-ghc984-aarch64-linux-android.sh
+++ build-ghc984-aarch64-linux-android.sh
@@ -24,45 +24,21 @@ fi
 cd ghc
 git submodule update --init --recursive
 
-cat > hadrian/settings.cabal <<EOF
-build-tool-depends:
-  alex:alex == 3.2.7.1,
-  happy:happy == 1.20.1.1
-EOF
-
-cat > mk/build.mk <<EOF
-BuildFlavour = quick-cross
-
-SRC_HC_OPTS += -O0
-GhcLibHcOpts += -O0
-
-Stage1Only = YES
-HADRIAN_ARGS += --docs=no-sphinx
-EOF
-
 export ANDROID_NDK_ROOT=/opt/android-ndk
 export ANDROID_TOOLCHAIN=/opt/android-toolchain
-
-export CC=aarch64-linux-android-clang
-export CXX=aarch64-linux-android-clang++
-export LD=aarch64-linux-android-ld
-export AR=aarch64-linux-android-ar
-export RANLIB=aarch64-linux-android-ranlib
-export STRIP=aarch64-linux-android-strip
 
 ./boot
 ./configure \
   --target=${TARGET} \
-  --with-gmp \
   --disable-numa \
-  --enable-unregisterised \
-  --enable-shared \
-  --enable-dynamic \
-  --with-iconv=no
+  --enable-unregisterised
 
 hadrian/build \
-  --flavour=quick-cross \
   --build-root=_build \
+  --flavour=quick-cross \
+  --docs=no-sphinx \
+  --docs=no-haddock \
+  --docs=no-man \
   binary-dist
 
 echo
